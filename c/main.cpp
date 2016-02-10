fbcd0a321f7533ab3387501d1d3638146f9a609c
dusty-nv <dustinf@nvidia.com>
2016-02-10 02:59:47 +0000

    Lua/C interop

--- c/main.cpp
+++ c/main.cpp
@@ -5,7 +5,43 @@
 #ifdef ROVERNET_CONSOLE
 
 #include "rovernet.h"
+#include <sys/stat.h>
 
+
+
+#define SCRIPT_FILENAME "main.lua"
+#define LUA_FUNC_NAME "ex_lua_func"
+
+void call_lua( lua_State* L )
+{
+	lua_getglobal(L, LUA_FUNC_NAME);
+	lua_pushnumber(L, 1.0);
+	lua_pushnumber(L, 2.0);
+
+	// call lua func (2 arguments, 1 result)
+	const int f_result = lua_pcall(L, 2, 1, 0);
+
+	printf("%s() ran (res=%i)\n", LUA_FUNC_NAME, f_result);
+	
+	if( f_result != 0 )
+	{
+		printf("error running %s()  %s\n", LUA_FUNC_NAME, lua_tostring(L, -1));
+		return;
+	}
+
+	// return value
+	const double ret = lua_tonumber(L, -1);
+	lua_pop(L, 1);
+	printf("%s() => %lf\n", LUA_FUNC_NAME, ret);
+}
+
+
+int lua_c_square( lua_State* L )
+{
+	double d = lua_tonumber(L, 1);	// get argument
+	lua_pushnumber(L, d * d );
+	return 1;
+};
 
 
 bool init()
@@ -23,6 +59,25 @@ bool init()
 	luaL_openlibs(L);
 	printf("opened lua libraries\n");
 	
+
+	lua_pushcfunction(L, lua_c_square);
+	lua_setglobal(L, "my_square");
+
+
+ 	// load and run file
+	const int res = luaL_dofile(L, SCRIPT_FILENAME);
+
+	if( res == 1 ) 
+	{
+		printf("Error executing resource: %s\n", SCRIPT_FILENAME);
+		const char* luastr = lua_tostring(L,-1);
+
+		if( luastr != NULL )
+			printf("%s\n", luastr);
+	}
+
+	call_lua(L);
+
 	printf("closing lua_State\n");
 	lua_close(L);
 
@@ -44,3 +99,4 @@ int main( int argc, char** argv )
 }
 
 #endif
+
--- lua/main.lua
+++ lua/main.lua
@@ -2,15 +2,18 @@
 -- 
 
 require 'torch'
+require 'ffi'
 
 
-function ex_lua_func()
-   ret = 'Called function inside lua'
-   print()
-   return ret
+function ex_lua_func( x, y )
+	print('HELLO from function inside LUA')
+	print(x)
+	print(y)
+	return x * y
 end
 
 print("Hello from Lua")
+print(my_square(4))
 
 torch.setdefaulttensortype('torch.FloatTensor')
 test_tensor = torch.rand(3, 4, 2)
