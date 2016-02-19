80f78a763e376b33eddafeeb22fa73d10615ab76
dusty-nv <dustinf@nvidia.com>
2016-02-19 19:28:07 +0000

    tensor update

--- cuda/cudaMappedMemory.h
+++ cuda/cudaMappedMemory.h
@@ -22,6 +22,7 @@ inline bool cudaAllocMapped( void** cpuPtr, void** gpuPtr, size_t size )
 	if( CUDA_FAILED(cudaHostGetDevicePointer(gpuPtr, *cpuPtr, 0)) )
 		return false;
 
+	memset(*cpuPtr, 0, size);
 	printf("[cuda]  cudaAllocMapped %zu bytes, CPU %p GPU %p\n", size, *cpuPtr, *gpuPtr);
 	return true;
 }
--- lua/rovernet.lua
+++ lua/rovernet.lua
@@ -36,7 +36,9 @@ r2 = nn.Sequential()
   
 criterion = nn.MSECriterion()
 
-
+inputs = torch.FloatTensor(2)
+target1 = torch.FloatTensor(1)
+target2 = torch.FloatTensor(1)
 print("Model1: ")
 print(r1)
 
@@ -49,37 +51,43 @@ print(r2)
 --TO DUSTIN: input tensors need to be [0,360] to function properly
 function update_network( imu_tensor, goal_tensor, output_tensor )
 
-	--print('[rovernet]  user_epoch(' .. os.clock() .. ')')
-	print( imu_tensor:size(1) .. ' ' .. goal_tensor:size(1) .. ' ' .. output_tensor:size(1) )
-	
 	bearing = imu_tensor[1][1]
-	goal  = goal_tensor[1][1]
-	print( 'bearing:  ' .. bearing )
-  print( 'goal:     ' .. goal)
+	goal    = goal_tensor[1][1]
+	print( 'bearing:  ' .. bearing  )
+  	print( 'goal:     ' .. goal)
   
-  --TO DUSTIN: Manage tensors for input here
-  output1 = r1:forward(inputs)
-  output2 = r2:forward(inputs)
-  
-  r1:zeroGradParameters()
-  r2:zeroGradParameters()
-  
-  --Targets are computed by calculating minDiff(goal_tensor)-theta(other). Tensors should be stored as floats and passed back
-  --For all of the fancy networks, this is really just linear regression to find the solution to two equations where we define
-  --the targets as the ideal solution to the other and utilize learning rate to slow down the solution to prevent oscillations
-  target1 = minDiff(goal_tensor, imu_tensor)-output2
-  target2 = minDiff(goal_tensor, imu_tensor)-output1
-  
-  --Online target calculation and backpropagation
-  gradOutputs = criterion:backward(output1, target1)
-  gradInputs = r1:backward(inputs, gradOutputs)
-  
-  r1:updateParameters(learningRate)
-  
-  gradOutputs = criterion:backward(output2, target2)
-  gradInputs = r2:backward(inputs, gradOutputs)
-  
-  r2:updateParameters(learningRate)
+	--TO DUSTIN: Manage tensors for input here
+	inputs[1] = bearing
+	inputs[2] = goal
+
+	output1 = r1:forward(inputs)
+	output2 = r2:forward(inputs)
+
+	print('motor1:  ' .. output1[1])
+	print('motor2:  ' .. output2[1])
+
+	output_tensor[1] = output1[1]
+	output_tensor[2] = output2[1]
+
+	r1:zeroGradParameters()
+	r2:zeroGradParameters()
+
+	--Targets are computed by calculating minDiff(goal_tensor)-theta(other). Tensors should be stored as floats and passed back
+	--For all of the fancy networks, this is really just linear regression to find the solution to two equations where we define
+	--the targets as the ideal solution to the other and utilize learning rate to slow down the solution to prevent oscillations
+	target1[1] = minDiff(goal, bearing)-output2[1]  --target1 = minDiff(goal_tensor, imu_tensor)-output2
+	target2[1] = minDiff(goal, bearing)-output1[1]  --target2 = minDiff(goal_tensor, imu_tensor)-output1
+
+	--Online target calculation and backpropagation
+	gradOutputs = criterion:backward(output1, target1)
+	gradInputs = r1:backward(inputs, gradOutputs)
+
+	r1:updateParameters(learningRate)
+
+	gradOutputs = criterion:backward(output2, target2)
+	gradInputs = r2:backward(inputs, gradOutputs)
+
+	r2:updateParameters(learningRate)
   
   --and iterate by sending a new goal_tensor to the function. It should be capable of generalizing.
 end
