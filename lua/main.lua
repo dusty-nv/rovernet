3b33cb6d8f59a16c549b86e88e980378036ddc2d
Michael Ghaben <michael@Michaels-MacBook-Pro-4.local>
2016-03-22 12:13:26 -0400

    Now loads network

--- lua/main.lua
+++ lua/main.lua
@@ -1,53 +1,27 @@
--- example test script
--- (do not use)
+--rovernet top-level script
 
 require 'torch'
-disp = require 'display'
+require 'cutorch'
+require 'nn'
+require 'cunn'
+require 'image'
 
-local ffi = require 'ffi'
+--What the saved network is named
+network = torch.load('network.t7')
 
-ffi.cdef[[
-	int printf(const char *fmt, ...);
-	void lua_ffi_msg( const char* str );
-	bool fillTensor(int rows, int cols, THByteTensor* emptyTensor);
-	THByteTensor* nextTensor(int rows, int cols);
-]]
-
-ffi.C.printf("ffi FFI Hello %s!\n", "world")
-
-disp.text('Welcome to ROVERNET')
-
-function ex_lua_func( x, y )
-	--print('HELLO from function inside LUA')
-	--print(x)
-	--print(y)
-	return x * y
-end
-
-function user_epoch( img_tensor )
-
-	ffi.C.printf('[roverNet]  user_epoch(%f)\n', os.clock())
-
-	img_dim    = img_tensor:dim()
-	img_width  = img_tensor:size(1)	-- indexes start at 1 ;)
-	img_height = img_tensor:size(2)
-
-	ffi.C.printf('[roverNet]  dims %0.0f width %0.0f height %0.0f\n', img_dim, img_width, img_height)
-	print(img_tensor)
-	
-end
-
---print("Hello from Lua")
---print(my_square(4))
+--Network init
+print('[rovernet] hello from within Torch/Lua environment (time=' .. 
+os.clock() .. ')')
 
 torch.setdefaulttensortype('torch.FloatTensor')
---test_tensor = torch.rand(3, 4, 2)
---print(test_tensor)
 
---ffi.C.lua_ffi_msg("AL0HA!")
+print('[rovernet] loading previously trained network (time=' .. 
+os.clock() .. ')')
+Agent = torch.load(network)
 
---test_tensor2 = torch.ByteTensor()
---ffi.C.fillTensor(4, 5, test_tensor2:cdata())
---print(test_tensor2)
---print(torch.numel(test_tensor2))
---print(torch.trace(test_tensor2))
+--Check agent
+print(Agent)
+Agent:forward(image)
+
+
+end
